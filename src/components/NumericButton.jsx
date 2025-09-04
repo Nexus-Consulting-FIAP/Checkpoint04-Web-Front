3587fe6c21f70df7f413afe9a7ac387b1fb2e07a
Daniel <danieloliveiradesouza09@outlook.com>
2025-09-03 21:03:40 -0300

    fix typo

--- src/components/NumericButton.jsx
+++ src/components/NumericButton.jsx
@@ -2,7 +2,7 @@ function NumericButton({ valor }) {
     return (
 
         <>
-            <div className=" cursor-pointer bg-amber-900 rounded-4xl text-center w-10 p-3 drop-shadow-sm"> 
+            <div className="cursor-pointer bg-amber-900 rounded-4xl text-center w-10 p-3 drop-shadow-sm"> 
                 <h1 className="text-white">{valor}</h1>
             </div>
         </>
