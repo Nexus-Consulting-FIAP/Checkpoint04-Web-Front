fc020f4d0a9a686a14330a512b2006c1b2977fff
Palermo <joaopedro1633@gmail.com>
2025-09-04 23:38:17 -0300

    Melhorando a aparência da calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -33,37 +33,46 @@ function App() {
     }
 
   }
-
-  return (
-    <>
-      <div className="roboto-sitefont justify-items-center">
+return (
+  <>
+    <div className="flex items-center justify-center min-h-screen bg-gray-300">
+      
+      
+      <div className="bg-gray-100 shadow-2xl rounded-2xl p-6 w-110 roboto-sitefont">
+        
+        
         <Display conta={conta || 0} resultado={resultado} />
-        <div className="grid grid-cols-4 gap-2">
+
+        
+        <div className="grid grid-cols-4 mt-4 m-0">
           <NumericButton valor={"AC"} cor_botao={operations_color.background} cor_texto={operations_color.font} onClick={() => gerenciarClique("AC")}/>
           <NumericButton valor={"+-"} cor_botao={operations_color.background} cor_texto={operations_color.font} onClick={() => gerenciarClique("+-")}/>
           <NumericButton valor={"%"} cor_botao={operations_color.background} cor_texto={operations_color.font} onClick={() => gerenciarClique("%")}/>
           <NumericButton valor={"X"} cor_botao={operations_color.background} cor_texto={operations_color.font} onClick={() => gerenciarClique("*")}/>
+          
           <NumericButton valor={7} cor_botao={numbers_color.background} cor_texto={numbers_color.font} onClick={() => gerenciarClique("7")}/>
           <NumericButton valor={8} cor_botao={numbers_color.background} cor_texto={numbers_color.font} onClick={() => gerenciarClique("8")}/>
           <NumericButton valor={9} cor_botao={numbers_color.background} cor_texto={numbers_color.font} onClick={() => gerenciarClique("9")}/>
           <NumericButton valor={"/"} cor_botao={operations_color.background} cor_texto={operations_color.font} onClick={() => gerenciarClique("/")}/>
+          
           <NumericButton valor={4} cor_botao={numbers_color.background} cor_texto={numbers_color.font} onClick={() => gerenciarClique("4")}/>
           <NumericButton valor={5} cor_botao={numbers_color.background} cor_texto={numbers_color.font} onClick={() => gerenciarClique("5")}/>
           <NumericButton valor={6} cor_botao={numbers_color.background} cor_texto={numbers_color.font} onClick={() => gerenciarClique("6")}/>
           <NumericButton valor={"-"} cor_botao={operations_color.background} cor_texto={operations_color.font} onClick={() => gerenciarClique("-")}/>
+          
           <NumericButton valor={1} cor_botao={numbers_color.background} cor_texto={numbers_color.font} onClick={() => gerenciarClique("1")}/>
           <NumericButton valor={2} cor_botao={numbers_color.background} cor_texto={numbers_color.font} onClick={() => gerenciarClique("2")}/>
           <NumericButton valor={3} cor_botao={numbers_color.background} cor_texto={numbers_color.font} onClick={() => gerenciarClique("3")}/>
           <NumericButton valor={"+"} cor_botao={operations_color.background} cor_texto={operations_color.font} onClick={() => gerenciarClique("+")}/>
+          
           <NumericButton valor={"x²"} cor_botao={operations_color.background} cor_texto={operations_color.font} onClick={() => gerenciarClique("**2")}/>
           <NumericButton valor={0} cor_botao={numbers_color.background} cor_texto={numbers_color.font} onClick={() => gerenciarClique("0")}/>
           <NumericButton valor={"."} cor_botao={operations_color.background} cor_texto={operations_color.font} onClick={() => gerenciarClique(".")}/>
           <NumericButton valor={"="} cor_botao={"bg-black"} cor_texto={"text-white"} onClick={() => gerenciarClique("=")}/>
         </div>
       </div>
-
-    </>
-  )
+    </div>
+  </>
+)
 }
-
 export default App
--- src/components/NumericButton.jsx
+++ src/components/NumericButton.jsx
@@ -1,12 +1,11 @@
 function NumericButton({ valor, cor_botao, cor_texto, onClick }) {
-    cor_botao += " drop-shadow-lg cursor-pointer rounded-full w-15 h-12 md:w-23 md:h-17 p-3 place-items-center hover:bg-purple-600"
-    
-    return (
-        <>
-            <button onClick={onClick} className={cor_botao}>
-                <h1 className={cor_texto}>{valor}</h1>
-            </button>
-        </>
-    )
+  cor_botao += " drop-shadow-lg cursor-pointer rounded-full w-15 h-12 md:w-23 md:h-17 p-3 place-items-center hover:bg-purple-600 m-1"
+
+  return (
+    <button onClick={onClick} className={cor_botao}>
+      <h1 className={cor_texto}>{valor}</h1>
+    </button>
+  )
 }
+
 export default NumericButton
